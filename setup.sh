6da7223ec7c810da65f1700014dd307369377366
David Li <dfli@google.com>
2012-06-29 17:10:33 -0700

    Added new setups and fixed an issue with automatically loading submodules

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 ln -fsv $DIR/.vim ~
 ln -fsv $DIR/.vimrc ~/.vimrc
+ln -fsv $DIR/.zsh ~/.zsh
 ln -fsv $DIR/.zshrc ~/.zshrc
 ln -fsv $DIR/.tmux.conf ~/.tmux.conf
 
@@ -9,5 +10,6 @@ ln -fsv $DIR/.tmux.conf ~/.tmux.conf
 git config --global user.name "David Li"
 git config --global color.ui true
 git config --global format.pretty oneline
+git submodule init
 git submodule sync
 git submodule update
